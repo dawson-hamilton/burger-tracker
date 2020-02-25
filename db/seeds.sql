0659759e49acdf6d1185725e005a0bf6b4786146
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-02-25 11:06:47 -0700

    adding the schema and the seeds for the database into the code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ("double double animal style");
+INSERT INTO burgers (name) VALUES ("big mac");
+INSERT INTO burgers (name) VALUES ("double baconator");
+INSERT INTO burgers (name) VALUES ("whopper");
